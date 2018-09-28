1d39bf5314d56517ede8c59ecab73e605609129e
Alex Scarlett <alex.scarlett91@gmail.com>
2018-09-28 05:33:39 -0400

    Moved all relevant helper files with functions to a helpers directory due to app file becoming cluttered

---
+++ helpers/data.py
@@ -0,0 +1,41 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+            pariatur.',
+            'author': 'Alex Scarlett',
+            'create_date': '09-27-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+            pariatur.',
+            'author': 'Alice Smith',
+            'create_date': '09-27-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+            pariatur.',
+            'author': 'John Doe',
+            'create_date': '09-27-2018'
+        }
+    ]
+
+    return articles
