b7240b40cceb0b3275c1b531787beedbc6ce60f9
Alex Scarlett <alex.scarlett91@gmail.com>
2018-09-27 20:53:58 -0400

    Created routes and accompanying functions to render templates for the home page, the list of all articles page, and for an individual article

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 
-@app.route('/')
+Articles = Articles()
 
-def hello_world():
-    return 'Hello World'
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about_us():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+@app.route('/article/<string:id>/')
+def show_article(id):
+    return render_template('article.html', id=id)
 
 if __name__ == '__main__':
     app.run()
